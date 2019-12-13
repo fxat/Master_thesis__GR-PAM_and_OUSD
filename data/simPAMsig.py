f3a19a39c8ef7da9814ef13e31e0c9f9e97f2ba5
franz taffner <FranzTaffner@franzs-MacBook-Pro.local>
2019-12-13 21:08:29 +0100

    created pam sig simulation

---
+++ data/simPAMsig.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+__author__ = "Franz Taffner"
+__copyright__ = "Copyright 2019"
+__license__ = "MIT"
+__version__ = "0.1"
+__email__ = "franz.taffner@yahoo.de"
+__status__ = "Simulation"
+
+"""
+
+"""
+
+import numpy as np
+import matplotlib 
+
+
+
+
+class NumberList(list):
+    pass    #much code deleted
+class FrequencyDistribution(dict):
+    pass    #much code deleted
+
+if __name__ == '__main__':    #code to execute if called from command-line
+    pass    #do nothing - code deleted
+#use this either for a simple example of how to use the module,
+#or when the module can meaningfully be called as a script.
