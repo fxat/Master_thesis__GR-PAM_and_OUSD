9bdd22ef1cd75cc3a267692193c7927a182bb43b
franz taffner <FranzTaffner@franzs-MacBook-Pro.local>
2019-12-16 22:07:43 +0100

    refractoring simPAMsig and transfer of constants

--- data/simPAMsig/plots/simPlots.py
+++ data/simPAMsig/plots/simPlots.py
@@ -17,30 +17,36 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.signal as sps
 
+from .. import constants as C
+
 class pamPlots:
 
     def plot_pP(self, ax):
         ax.set_title('Pressure signal: $\delta$-peak excitation')
         ax.set_xlabel('Distance in m')
         ax.set_ylabel('Preassure in Pa')
+        ax.set_xlim((C.z-5*C.a), (C.z+5*C.a))
         ax.grid()
 
     def plot_laserPulse(self, ax):
         ax.set_title('Temporal profile of a laser pulse')
         ax.set_xlabel('Time in s')
         ax.set_ylabel('Laser power in N.A.')
+        ax.set_xlim(-2*tp, 2*tp)
         ax.grid()
 
     def plot_sigSphere(self, ax):
         ax.set_title('Ideal measured signal')
         ax.set_xlabel('Distance in m')
         ax.set_ylabel('N.A.')
+        ax.set_xlim(C.z-3*C.a, C.z+3*C.a)
         ax.grid()
 
     def plot_fftSigSphere(self, ax):
         ax.set_title('Frequency spectrum of an ideal Signal')
         ax.set_xlabel('f in Hz')
         ax.set_ylabel('N.A')
+        ax.set_xlim(-200e6, 200e6)
         ax.grid()
 
     def plot_sensor(self, ax):
@@ -48,6 +54,7 @@ class pamPlots:
         ax.legend(['Pressure wave spectrum', 'Resulting signal spectrum', 'Transducer transferfunction'])
         ax.set_xlabel('f in Hz')
         ax.set_ylabel('N.A.')
+        ax.set_xlim(-200e6, 200e6)
         ax.grid()
 
     def plot_transSigTemp(self, ax):
@@ -55,5 +62,6 @@ class pamPlots:
         ax.legend(['Bandwidth limited detector', 'Pointlike detector'])
         ax.set_xlabel('$c_st$ in m')
         ax.set_ylabel('p in Pa')
+        ax.set_xlim(C.z-10*C.a, C.z+50*C.a)
         ax.grid()
     
--- data/simPAMsig/simPAMsig.py
+++ data/simPAMsig/simPAMsig.py
@@ -17,71 +17,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.signal as sps
+import constants as C
 
 from plots.simPlots import pamPlots
 
 #%%
-#####[ DEFINES ]###############################################################
 
-N = int(1e5) # Number of datapoints
+p0 = C.F * C.mu_a * C.gamma   # Initial preasure rise
 
-cs = 1500       # Speed of sound in water [m/s]
-a = 10e-6       # Radius of a spherical source [m]
-z = 10e-3       # Distance source <-> detector [m]
-
-F = 200         # Fluence [J/m]
-mu_a = 20000    # Absorption coefficient [1/m]
-gamma = 0.11    # Grueneisen Parameter
-
-#####[ PRECONDITIONS ]#########################################################
-
-p0 = F * mu_a * gamma   # Initial preasure rise
-
-t_max = 2 * z / cs
-t = np.linspace(0, t_max, N)    # Time intervall
-freq = np.linspace(0, (N)/np.max(t), len(t))
+t = np.linspace(0, C.t_max, C.N)    # Time intervall
+freq = np.linspace(0, (C.N)/np.max(t), len(t))
 freq = freq - np.max(freq)/2
 
 pamSimPlots = pamPlots()
 
 #%% Delta pulse excitation of a shere
 
-data = {'dist': cs*t, 'preasure': np.zeros(N)}
+data = {'dist': C.cs*t, 'preasure': np.zeros(C.N)}
 pP = pd.DataFrame(data, dtype=float)
 
-pP['preasure']= np.piecewise(t, [np.logical_and(cs*t>(z-a), cs*t<(z+a))], 
-                                [lambda t: 0.5*(z-cs*t)/z])
+pP['preasure']= np.piecewise(t, [np.logical_and(C.cs*t>(C.z-C.a), C.cs*t<(C.z+C.a))], 
+                                [lambda t: 0.5*(C.z-C.cs*t)/C.z])
 
 ax_pP = pP.plot(x='dist', y='preasure')
 pamSimPlots.plot_pP(ax_pP)
-ax_pP.set_xlim((z-5*a), (z+5*a))
 
 #%% Gaussian temporal profil of a excitation laser pulse
 
-tp = 10e-9      # Temporal laser pulse width [ns] 
-t0 = t_max/2
 
-sigma = tp/(2*np.sqrt(2*np.log(2)))
-LP = np.exp(-(t-t0)**2/(2*sigma**2))
+sigma = C.tp/(2*np.sqrt(2*np.log(2)))
+LP = np.exp(-(t-C.t0)**2/(2*sigma**2))
 
-t_shift = t-t_max/2
+t_shift = t-C.t_max/2
 laserPulseData = {'lPtime': t_shift, 'lPpowerNormed': LP}
 
 laserPulse = pd.DataFrame(laserPulseData, dtype=float)
 ax_laserPulse = laserPulse.plot(x='lPtime', y='lPpowerNormed')
 pamSimPlots.plot_laserPulse(ax_laserPulse)
-ax_laserPulse.set_xlim(-2*tp, 2*tp)
 
 #%% Signal of a sphere with finite excitation pulse duration
 
 sigSpherePulse = np.convolve(pP['preasure'], laserPulse['lPpowerNormed'], mode='same')
-sigSphereData = {'dist': cs*t, 'sigSphere': sigSpherePulse}
+sigSphereData = {'dist': C.cs*t, 'sigSphere': sigSpherePulse}
 sigSphere = pd.DataFrame(sigSphereData, dtype=float)
 
 ax_sigSphere = sigSphere.plot(x='dist', y='sigSphere')
 pP.plot(x='dist', y='preasure', ax=ax_sigSphere)
 pamSimPlots.plot_sigSphere(ax_sigSphere)
-ax_sigSphere.set_xlim(z-3*a, z+3*a)
 
 #%% Spectrum of the spherical signal
 
@@ -99,7 +81,6 @@ fft_sigSphere = pd.DataFrame(fft_sigSphereData, dtype=float)
 
 ax_fftSigSphere = fft_sigSphere.plot(x='freq', y='absfftSigSphere')
 pamSimPlots.plot_fftSigSphere(ax_fftSigSphere)
-ax_fftSigSphere.set_xlim(-200e6, 200e6)
 
 #%% Construct sensor transfer function
 
@@ -135,12 +116,11 @@ ax_sensor = sensor.plot(x='freq', y='as')
 transducerSig.plot(x='freq', y='specTransNorm', ax=ax_sensor)
 fft_sigSphere.plot(x='freq', y='fftSigSphereNorm', ax=ax_sensor) 
 pamSimPlots.plot_sensor(ax_sensor)
-ax_sensor.set_xlim(-200e6, 200e6)
 
 #%%
 
 specResultTransducer_temp = np.fft.ifft(np.fft.ifftshift(specResultTransducer)).real
-specResultTransducerData_temp = {'dist': cs*t, 
+specResultTransducerData_temp = {'dist': C.cs*t, 
                                 'tempSig': p0*specResultTransducer_temp,
                                 'sigSphere': p0*sigSphere['sigSphere']}
 transducerSig_temp = pd.DataFrame(specResultTransducerData_temp, dtype=float)
@@ -148,7 +128,6 @@ transducerSig_temp = pd.DataFrame(specResultTransducerData_temp, dtype=float)
 ax_transSigTemp = transducerSig_temp.plot(x='dist', y='tempSig')
 transducerSig_temp.plot(x='dist', y='sigSphere', ax=ax_transSigTemp)
 pamSimPlots.plot_transSigTemp(ax_transSigTemp)
-ax_transSigTemp.set_xlim(z-10*a, z+50*a)
 
 #%%
 
